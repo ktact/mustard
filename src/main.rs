cbefcda614191d4c617725ee6294e12b19ef4122
ktact <ktact.dev@gmail.com>
2025-04-30 11:34:57 +0900

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -108,10 +108,7 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
     let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
     let vram = unsafe {
-        slice::from_raw_parts_mut(
-            vram_addr as *mut u32,
-            vram_byte_size / size_of::<u32>(),
-        )
+        slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
     };
     for e in vram {
         *e = 0xffffff;
@@ -120,7 +117,6 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         hlt()
     }
 }
-
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
