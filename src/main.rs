3ff8e41cf0aa826cbdf52cddaf8ef1854c0e9b22
ktact <ktact.dev@gmail.com>
2025-05-02 20:20:49 +0900

    Implement memory allocator

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,14 @@ use core::panic::PanicInfo;
 use mustard::graphics::draw_test_pattern;
 use mustard::graphics::fill_rect;
 use mustard::graphics::Bitmap;
+use mustard::init::init_basic_runtime;
 use mustard::qemu::exit_qemu;
 use mustard::qemu::QemuExitCode;
 use mustard::serial::SerialPort;
-use mustard::uefi::exit_from_efi_boot_services;
 use mustard::uefi::init_vram;
 use mustard::uefi::EfiHandle;
 use mustard::uefi::EfiMemoryType;
 use mustard::uefi::EfiSystemTable;
-use mustard::uefi::MemoryMapHolder;
 use mustard::uefi::VramTextWriter;
 
 use mustard::x86::hlt;
@@ -30,14 +29,7 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     fill_rect(&mut vram, 0x000000, 0, 0, vw, vh).expect("fill_rect failed");
     draw_test_pattern(&mut vram);
     let mut w = VramTextWriter::new(&mut vram);
-    for i in 0..4 {
-        writeln!(w, "i = {i}").unwrap();
-    }
-    let mut memory_map = MemoryMapHolder::new();
-    let status = efi_system_table
-        .boot_services()
-        .get_memory_map(&mut memory_map);
-    writeln!(w, "{status:?}").unwrap();
+    let memory_map = init_basic_runtime(image_handle, efi_system_table);
     let mut total_memory_pages = 0;
     for e in memory_map.iter() {
         if e.memory_type() != EfiMemoryType::CONVENTIONAL_MEMORY {
@@ -52,7 +44,6 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         "Total: {total_memory_pages} pages = {total_memory_size_mib} MiB"
     )
     .unwrap();
-    exit_from_efi_boot_services(image_handle, efi_system_table, &mut memory_map);
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
 
     loop {
